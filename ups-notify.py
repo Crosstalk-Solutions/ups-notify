f4f3e8270a1c5297aa055eee7e85f613b93aad0c
techied <11001514+techied@users.noreply.github.com>
2023-07-24 13:21:56 -0700

    send discord webhook

--- ups-notify.py
+++ ups-notify.py
@@ -8,6 +8,8 @@ import os
 GPIO.setmode(GPIO.BCM)
 # Choose a GPIO pin to use to read the UPS status
 LO_DT_PIN = 17
+DISCORD_WEBHOOK_URL = 'REPLACE WITH YOUR DISCORD WEBHOOK URL'
+DISCORD_USER_ID = '408878638109425665'
 # When the LO_DT pin is high, the UPS battery voltage is low
 # Set the chosen pin to be an input pin and enable the internal pull-up resistor
 GPIO.setup(LO_DT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -15,6 +17,8 @@ GPIO.setup(LO_DT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def low_battery():
     print('battery is low')
+    # send a discord webhook
+    os.system('curl -H "Content-Type: application/json" -X POST -d \'{"content": "<@' + DISCORD_USER_ID + '> UPS battery is low. Shutting down the system in 30 seconds."}\' ' + DISCORD_WEBHOOK_URL)
     # send a message to the user
     os.system('echo "UPS battery is low. Shutting down the system in 30 seconds." | wall')
     time.sleep(30)
