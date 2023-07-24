a7a1d50dd83e87c7ee96c49246a028c6f558bb15
techied <11001514+techied@users.noreply.github.com>
2023-07-24 13:25:32 -0700

    install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+echo "Installing UPS Notify..."
+
+echo "Copying service script"
+
+cp ups-notify.service /etc/systemd/system/ 2>/dev/null
+
+echo "Reloading systemd"
+
+systemctl daemon-reload 2>/dev/null
+
+echo "Enabling and starting service"
+
+systemctl enable ups-notify 2>/dev/null
+systemctl start ups-notify 2>/dev/null
+
+echo "Installation complete."
