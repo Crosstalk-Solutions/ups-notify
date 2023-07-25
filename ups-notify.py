cdcb288569e1c49bb66ae02d88579ae612a25876
techied <11001514+techied@users.noreply.github.com>
2023-07-25 10:44:59 -0700

    use pull down resistor

--- ups-notify.py
+++ ups-notify.py
@@ -11,7 +11,7 @@ LO_DT_PIN = 17
 DISCORD_WEBHOOK_URL = 'REPLACE WITH YOUR DISCORD WEBHOOK URL'
 DISCORD_USER_ID = '408878638109425665'
 # When the LO_DT pin is high, the UPS battery voltage is low
-GPIO.setup(LO_DT_PIN, GPIO.IN)
+GPIO.setup(LO_DT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 
 def low_battery():
